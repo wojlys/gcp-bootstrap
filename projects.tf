b3c6af34a298f96058f26935fa99ee8c0002d7b9
Wojtek <wojlys2@gmail.com>
2026-05-04 13:56:26 +0200

    re-run with org policy set to prevent default vpc to be created in new projects

--- projects.tf
+++ projects.tf
@@ -22,25 +22,26 @@ resource "random_string" "compute_suffix" {
 
 ######################
 # Projects to be created under parent folder
+# Notice!
+# Default network creation is disabled via org policy
 ######################
 
 resource "google_project" "network" {
-  name                = format("rss-network-%s", random_string.network_suffix.result)
-  project_id          = format("rss-network-%s", random_string.network_suffix.result)
-  folder_id           = var.parent_folder_id
-  billing_account     = var.this_billing["network"]
-  auto_create_network = false
+  name            = format("rss-network-%s", random_string.network_suffix.result)
+  project_id      = format("rss-network-%s", random_string.network_suffix.result)
+  folder_id       = var.parent_folder_id
+  billing_account = var.this_billing["network"]
   # allow terraform to delete project on destroy
   deletion_policy = "DELETE"
+
 }
 
 
 resource "google_project" "compute" {
-  name                = format("rss-compute-%s", random_string.compute_suffix.result)
-  project_id          = format("rss-compute-%s", random_string.compute_suffix.result)
-  folder_id           = var.parent_folder_id
-  billing_account     = var.this_billing["compute"]
-  auto_create_network = false
+  name            = format("rss-compute-%s", random_string.compute_suffix.result)
+  project_id      = format("rss-compute-%s", random_string.compute_suffix.result)
+  folder_id       = var.parent_folder_id
+  billing_account = var.this_billing["compute"]
   # allow terraform to delete project on destroy
   deletion_policy = "DELETE"
 }
